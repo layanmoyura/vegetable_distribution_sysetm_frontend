b4259b1db91ad54a010f2f47b0713f4ccf3ebe55
layanmoyura <layanmoyura@gmail.com>
2022-12-24 03:06:56 +0530

    payment validations

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit,Output,EventEmitter } from '@angular/core';
-
+import { AbstractControl, UntypedFormBuilder, UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
@@ -8,12 +9,66 @@ import { Component, OnInit,Output,EventEmitter } from '@angular/core';
 export class CheckoutComponent implements OnInit {
 @Output() public childEvent= new EventEmitter();
 public Activepay:boolean=true;
-  constructor() { }
+  constructor(private formBuilder:UntypedFormBuilder,private toastr:ToastrService) { }
+  form: UntypedFormGroup = new UntypedFormGroup({
+    
+    
+  });
+
+  submitted=false;
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group(
+      {
+
+       
+        No: [
+          '',
+          [
+            Validators.required
+            
+          ]
+        ],
+
+        Name: [
+          '',
+          [
+            Validators.required
+            
+          ]
+        ],
+
+        PIN: [
+          '',
+          [
+            Validators.required
+            
+          ]
+        ],
+
+
+        
+
+        
+
+      },
+
+    );
+
+
   }
 
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+
+  
+
   pay(){
+    this.submitted = true;
+  if (this.form.invalid) {
+    return;
+  }
     this.childEvent.emit(true);
     this.Activepay=false;
   }
